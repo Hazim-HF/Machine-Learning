c6284c126a993357d123dda209c9216ffe889b6e
Hazim-HF <hazim.fitri.hf@gmail.com>
2025-07-09 14:07:32 +0800

    update

--- ML.py
+++ ML.py
@@ -131,3 +131,5 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 lda = LinearDiscriminantAnalysis(n_components=2)
 X_train = lda.fit_transform(X_train, y_train)
 X_test = lda.transform(X_test)
+
+# ===== Decision Tree =====
---
+++ PyCharm/Machine-Learnin.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+
+
+
+!pip install pandas
+
+
